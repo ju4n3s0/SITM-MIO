44905c031aa654ce90765c4ddce2a3143fafa4cb
ju4n3s0 <juanestebansotelomera@gmail.com>
2025-11-28 23:01:18 -0500

    structure

--- DataCenter/src/main/java/db/ManageDatabase.java
+++ DataCenter/src/main/java/db/ManageDatabase.java
@@ -1,17 +1,18 @@
-package db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DatabaseManager {
+public class ManageDatabase {
 
-    private static final String URL =
+    private static final String URL = 
         "jdbc:postgresql://10.147.20.62:5432/SITM-MIOJM";
     private static final String USER = "postgres";
     private static final String PASSWORD = "postgres";
+    
 
-    public static Connection getConnection() throws SQLException {
+    public static Connection gConnection() throws SQLException{
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
-} 
+
+}
