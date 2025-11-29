2a02d1ef8d1abad9bf049e2fe1a26de4405ebaf0
John Hortua <1109660285@u.icesi.edu.co>
2025-11-29 18:01:03 -0500

    Added Main class initialization logic with UI launch and component wiring

--- OperationControl/src/main/java/com/sitm/mio/operationcontrol/Main.java
+++ OperationControl/src/main/java/com/sitm/mio/operationcontrol/Main.java
@@ -1,6 +1,9 @@
 package com.sitm.mio.operationcontrol;
 
+import com.sitm.mio.operationcontrol.component.*;
 import com.sitm.mio.operationcontrol.ui.OperationControlUI;
+
+import javax.swing.SwingUtilities;
 
 /**
  * Main entry point for the Operation Control System.
@@ -14,7 +17,37 @@ import com.sitm.mio.operationcontrol.ui.OperationControlUI;
 public class Main {
     
     public static void main(String[] args) {
-        // TODO: Launch JavaFX UI
-        // OperationControlUI.launch(OperationControlUI.class, args);
+        System.out.println("OperationControl System Starting...");
+        
+        // Launch UI on Event Dispatch Thread
+        SwingUtilities.invokeLater(() -> {
+            // Create UI
+            OperationControlUI ui = new OperationControlUI();
+            
+            // TODO: Initialize backend components
+            // String proxyUrl = System.getProperty("proxy.url", "http://localhost:8080");
+            // String wsUrl = System.getProperty("ws.url", "ws://localhost:8080/stream/operator");
+            
+            // ProxyClient proxyClient = new ProxyClient(proxyUrl);
+            // EventReceiver eventReceiver = new EventReceiver(wsUrl);
+            // ITaskDelegator taskDelegator = new TaskDelegator();
+            // IAlertSender alertSender = new AlertSender();
+            // IReportSender reportSender = new ReportSender();
+            // IAnalyticsClient analyticsClient = new AnalyticsClient();
+            
+            // Controller controller = new Controller(
+            //     proxyClient,
+            //     eventReceiver,
+            //     ui,  // UI implements IVisualizacion
+            //     taskDelegator,
+            //     alertSender,
+            //     reportSender,
+            //     analyticsClient
+            // );
+            
+            // ui.setController(controller);
+            
+            System.out.println("UI launched successfully");
+        });
     }
 }
