f3af92d2118547de1027cc3e7b2d24a974a8848e
John Hortua <1109660285@u.icesi.edu.co>
2025-11-30 18:49:48 -0500

    New shared DTO classes

---
+++ Citizen/src/main/java/com/sitm/mio/citizen/dto/GetCitizenInformationRequest.java
@@ -0,0 +1,24 @@
+package com.sitm.mio.citizen.dto;
+
+/**
+ * Data Transfer Object representing a request for citizen information.
+ * Contains origin and destination stop IDs for travel time queries.
+ */
+public class GetCitizenInformationRequest {
+
+    private final long originId;
+    private final long destinationId;
+
+    public GetCitizenInformationRequest(long originId, long destinationId) {
+        this.originId = originId;
+        this.destinationId = destinationId;
+    }
+
+    public long getOriginId() {
+        return originId;
+    }
+
+    public long getDestinationId() {
+        return destinationId;
+    }
+}
