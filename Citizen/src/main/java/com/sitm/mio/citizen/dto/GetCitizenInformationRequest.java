c0b0118d885c28b12dbaeb5f8635524ffd3bec54
John Hortua <1109660285@u.icesi.edu.co>
2025-11-30 21:24:35 -0500

    Added Jackson JSON annotations to GetCitizenInformationRequest DTO with @JsonCreator and @JsonProperty for originId and destinationId fields to enable proper JSON deserialization

--- Citizen/src/main/java/com/sitm/mio/citizen/dto/GetCitizenInformationRequest.java
+++ Citizen/src/main/java/com/sitm/mio/citizen/dto/GetCitizenInformationRequest.java
@@ -1,5 +1,8 @@
 package com.sitm.mio.citizen.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 /**
  * Data Transfer Object representing a request for citizen information.
  * Contains origin and destination stop IDs for travel time queries.
@@ -9,7 +12,13 @@ public class GetCitizenInformationRequest {
     private final long originId;
     private final long destinationId;
 
-    public GetCitizenInformationRequest(long originId, long destinationId) {
+    /**
+     * Constructor for Jackson deserialization.
+     */
+    @JsonCreator
+    public GetCitizenInformationRequest(
+            @JsonProperty("originId") long originId, 
+            @JsonProperty("destinationId") long destinationId) {
         this.originId = originId;
         this.destinationId = destinationId;
     }
