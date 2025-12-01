740f95bea991ab1f4d7e446831b739b0fee6e4a2
John Hortua <1109660285@u.icesi.edu.co>
2025-12-01 08:29:09 -0500

    Updated IDatagramReceiver interface documentation to clarify datagram reception from buses with event bus publishing for telemetry data, and corrected realizing class reference from ReceptorDatagramas to DatagramReceiver

--- DataCenter/src/main/java/com/sitm/mio/datacenter/interfaces/IDatagramReceiver.java
+++ DataCenter/src/main/java/com/sitm/mio/datacenter/interfaces/IDatagramReceiver.java
@@ -1,8 +1,10 @@
 package com.sitm.mio.datacenter.interfaces;
 
 /**
- * Interface for UDP datagram reception from buses.
- * Realized by: ReceptorDatagramas
+ * Interface for datagram reception from buses.
+ * Receives bus telemetry data and publishes events to the event bus.
+ * 
+ * Realized by: DatagramReceiver
  */
 public interface IDatagramReceiver {
     void start();
